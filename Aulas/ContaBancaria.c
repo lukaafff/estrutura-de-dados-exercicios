5e4d3b0ad51408c97034005c294dfdbc32635532
lukaafff <luizagarcia2405@gmail.com>
2023-08-14 20:15:02 -0300

    Update ContaBancaria.c

--- Aulas/ContaBancaria.c
+++ Aulas/ContaBancaria.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <string.h> //trabalhar com strings
+#include <stdlib.h> // Para usar a função system
 
 typedef struct {
+    char nome[50];
     int numero;
     double saldo;
 } contabancaria;
 
-void inicia (contabancaria* conta, int numero, double saldo) {
+void inicia (contabancaria* conta, char* nome, int numero, double saldo) {
+    //strncpy é uma função para copiar uma sequencia de caracters de uma fonte para um destino com um limite máximo
+    strncpy(conta->nome, nome, sizeof(conta->nome));
+    //        destino    fonte   numero de caracteres a serem copiados   
+     
     (*conta).numero = numero;
     (*conta).saldo = saldo;
 }
 
-void deposito(contabancaria* conta, double valor) {
+void deposito(contabancaria* conta) {
+    double valor;
+    printf("Qual valor deseja depositar?\n");
+    scanf("%lf", &valor);
     conta->saldo += valor;
 }
 
@@ -20,8 +30,16 @@ void deposito (contabancaria* conta, double valor) {
 }
 */
 
-void saque(contabancaria* conta, double valor) {
-    conta->saldo -= valor;
+void saque(contabancaria* conta) {
+    double valor;
+    printf("Qual valor deseja sacar?\n");
+    scanf("%lf", &valor);
+    if(valor > conta->saldo) {
+        printf("\n====== Valor para sacar e maior que o valor na conta! ======\n");
+    } else {
+        conta->saldo -= valor;
+        printf("\nSaque feito com sucesso!\n");
+    }
 }
 
 /*
@@ -31,18 +49,55 @@ void saque (contabancaria* conta, double valor) {
 */
 
 void imprime(contabancaria conta) {
-    printf("Numero Conta: %d\n", conta.numero);
-    printf("Saldo: %.2f\n", conta.saldo); //.2 formatação apos virgula
+    printf("======= CONTA BANCARIA ======= \n\n");
+    printf("Conta de: %s\n", conta.nome);
+    printf("Numero da conta: %d\n", conta.numero);
+    printf("Saldo disponivel: %.2f\n", conta.saldo); //.2 formatação apos virgula
 }
 
 int main() {
     contabancaria minhaConta;
-    inicia(&minhaConta, 2405, 0.0);
-
-    deposito(&minhaConta, 10);
-    saque(&minhaConta, 5);
+    inicia(&minhaConta, "Luiza", 24052003, 0.0);
 
     imprime(minhaConta);
 
+    int opcao;
+
+    while (1)
+    {
+        printf("\n===== SELECIONE UMA OPCAO =====");
+        printf("\n1 - Depositar");
+        printf("\n2 - Sacar");
+        printf("\n3 - Sair");
+        printf("\n===============================\n");
+
+        scanf("%d", &opcao);
+        switch (opcao)
+        {
+            case 1:
+                system("clear");
+                deposito(&minhaConta);
+                printf("\nDeposito feito com sucesso!\n");
+                imprime(minhaConta);
+                break;
+
+            case 2:
+                system("clear");
+                saque(&minhaConta);
+                imprime(minhaConta);
+                break;
+
+                case 3:
+                system("clear");
+                printf("Saindo da conta...");
+                return 0;
+                break;
+            
+            default:
+                 printf("Opcao invalida. Tente novamente.\n");
+                break;
+        }
+
+    }
     return 0;
 }

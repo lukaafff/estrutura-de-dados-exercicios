0a00953ce528df5899d9bf6ecafe4ba4e600c0a9
Luiza Garcia <85316771+lukaafff@users.noreply.github.com>
2023-08-24 19:35:55 -0300

    update

--- Pessoal/Calloc_.c
+++ Pessoal/Calloc_.c
@@ -6,18 +6,18 @@ int main () {
     int *vendasDiarias = (int *)calloc(diasMes, sizeof(int));
 
     if(vendasDiarias == NULL) {
-        printf(|"Erro na alocacao de memoria");
+        printf("Erro na alocacao de memoria");
         return 1;
     }
 
     //simular vendar (valores aleatorios)
     for (int i = 0; i < diasMes; i++ ) {
-        vendasDiarias[i] = rand() % 100; //entre 0 e 99
+        vendasDiarias[i] = rand() % 2000; //entre 0 e 99
     }
 
     //soma das vendas
     int totalVendas = 0;
-    for (int i = 0; i < diasMes; i ++) {
+    for (int i = 0; i < diasMes; i++) {
         totalVendas += vendasDiarias[i];
     }
 
